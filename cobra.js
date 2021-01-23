e0d059610ae5f243ceb734d71b6c3c52ccec0c28
ALUNO <alunoisaac624@gmail.com>
2021-01-23 12:21:51 -0300

    script das tecla

---
+++ cobra.js
@@ -0,0 +1,32 @@
+let tela;
+
+let ctx;
+
+let proxX;
+let proxY;
+
+window.onload - function () {
+    tela = document.getElementById("canvas");
+    ctx = tela.getContext("2d");
+    let vel = 8;
+    setInterval(desenharJogo, 1000 / vel);
+}
+
+function keyDownEvent(tecla) {
+    if (tecla = 37) {
+        proxX = -1;
+        proxY = 0;
+    }
+    if (tecla = 38) {
+        proxX = 0;
+        proxY = -1;
+    }
+    if (tecla = 39) {
+        proxX = 1;
+        proxY = 0;
+    }
+    if (tecla40) {
+        proxx = 0;
+        proxY = 1;
+    }
+}
